0a8d9b44bb45dbc57349552497d21adaeb73d373
rwenite <rwenite@gmail.com>
2017-12-07 18:22:33 -0500

    Update tests and package.json

--- tests/test.js
+++ tests/test.js
@@ -7,6 +7,7 @@ var moment = require('moment');
 var test = require('tape');
 
 const {spawn} = require('child_process');
+require('dotenv').config();
 
 // (test_info) Get package metadata
 var json = JSON.parse(fs.readFileSync('package.json', 'utf8'));
@@ -41,7 +42,7 @@ test('Tests for ' + json.name + ' (' + json.version + ')', t => {
 	// (test_help) Test show help
 	var child = spawn('node', ['./bin/twitter2pg', '-h']);
 	child.stderr.on('data', data => {
-		t.fail('(MAIN) help: ' + data);
+		t.fail('(MAIN) help: ' + data.toString('utf8'));
 	});
 	child.on('close', code => {
 		t.equals(code, 0, '(MAIN) help');
@@ -50,7 +51,7 @@ test('Tests for ' + json.name + ' (' + json.version + ')', t => {
 	// (test_doc_twitter2pg) Test show doc twitter2pg
 	var child = spawn('node', ['./bin/twitter2pg', 'doc', 'twitter2pg']);
 	child.stderr.on('data', data => {
-		t.fail('(MAIN) doc twitter2pg: ' + data);
+		t.fail('(MAIN) doc twitter2pg: ' + data.toString('utf8'));
 	});
 	child.on('close', code => {
 		t.equals(code, 0, '(MAIN) doc twitter2pg');
@@ -59,7 +60,7 @@ test('Tests for ' + json.name + ' (' + json.version + ')', t => {
 	// (test_doc_twitter) Test show doc twitter
 	var child = spawn('node', ['./bin/twitter2pg', 'doc', 'twitter']);
 	child.stderr.on('data', data => {
-		t.fail('(MAIN) doc twitter: ' + data);
+		t.fail('(MAIN) doc twitter: ' + data.toString('utf8'));
 	});
 	child.on('close', code => {
 		t.equals(code, 0, '(MAIN) doc twitter');
@@ -68,7 +69,7 @@ test('Tests for ' + json.name + ' (' + json.version + ')', t => {
 	// (test_doc_pg) Test show doc pg
 	var child = spawn('node', ['./bin/twitter2pg', 'doc', 'pg']);
 	child.stderr.on('data', data => {
-		t.fail('(MAIN) doc pg: ' + data);
+		t.fail('(MAIN) doc pg: ' + data.toString('utf8'));
 	});
 	child.on('close', code => {
 		t.equals(code, 0, '(MAIN) doc pg');
@@ -77,7 +78,7 @@ test('Tests for ' + json.name + ' (' + json.version + ')', t => {
 	// (test_file) Test create env file
 	var child = spawn('node', ['./bin/twitter2pg', 'file', './tests/out/.env']);
 	child.stderr.on('data', data => {
-		t.fail('(MAIN) env: ' + data);
+		t.fail('(MAIN) env: ' + data.toString('utf8'));
 	});
 	child.on('close', code => {
 		t.equals(code, 0, '(MAIN) env');
@@ -86,7 +87,7 @@ test('Tests for ' + json.name + ' (' + json.version + ')', t => {
 	// (test_set) Test set default
 	var child = spawn('node', ['./bin/twitter2pg', 'set', 'file', './tests/out/.env']);
 	child.stderr.on('data', data => {
-		t.fail('(MAIN) set: ' + data);
+		t.fail('(MAIN) set: ' + data.toString('utf8'));
 	});
 	child.on('close', code => {
 		t.equals(code, 0, '(MAIN) set');
@@ -94,7 +95,7 @@ test('Tests for ' + json.name + ' (' + json.version + ')', t => {
 		// (test_delete) Test delete default
 		var child = spawn('node', ['./bin/twitter2pg', 'delete', 'file']);
 		child.stderr.on('data', data => {
-			t.fail('(MAIN) delete: ' + data);
+			t.fail('(MAIN) delete: ' + data.toString('utf8'));
 		});
 		child.on('close', code => {
 			t.equals(code, 0, '(MAIN) delete');
@@ -104,20 +105,28 @@ test('Tests for ' + json.name + ' (' + json.version + ')', t => {
 	// (test_query_create) Test query CREATE
 	var child = spawn('node', ['./bin/twitter2pg', 'query', 'CREATE TABLE IF NOT EXISTS twitter_data(tweets jsonb);']);
 	child.stderr.on('data', data => {
-		t.fail('(MAIN) query CREATE: ' + data);
+		t.fail('(MAIN) query CREATE: ' + data.toString('utf8'));
 	});
 	child.on('close', code => {
 		t.equals(code, 0, '(MAIN) query CREATE');
 		
-		// (test_query_drop) Test query DROP
-		var child = spawn('node', ['./bin/twitter2pg', 'query', 'DROP TABLE twitter_data;']);
+		// (test_twitter2pg) Test twitter2pg
+		var child = spawn('node', ['./bin/twitter2pg']);
 		child.stderr.on('data', data => {
-			t.fail('(MAIN) query DROP: ' + data);
+			t.fail('(MAIN) twitter2pg: ' + data.toString('utf8'));
 		});
 		child.on('close', code => {
-			t.equals(code, 0, '(MAIN) query DROP');
+			
+			// (test_query_drop) Test query DROP
+			var child = spawn('node', ['./bin/twitter2pg', 'query', 'DROP TABLE twitter_data;']);
+			child.stderr.on('data', data => {
+				t.fail('(MAIN) query DROP: ' + data.toString('utf8'));
+			});
+			child.on('close', code => {
+				t.equals(code, 0, '(MAIN) query DROP');
+			});
+			t.equals(code, 0, '(MAIN) twitter2pg');
 		});
 	});
-	
 	t.end();
 });
